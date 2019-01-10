59ef08d80e5994661266814f5ff3bd2fd54690a4
BeijiYang <langyuyang@outlook.com>
2019-01-10 20:24:19 +0800

    refactor

--- pages/switchcity/switchcity.js
+++ pages/switchcity/switchcity.js
@@ -1,60 +1,50 @@
-const city = require('../../utils/city.js');
-const cityObjs = require('../../utils/city.js');
-const config = require('../../utils/config.js');
-const utils = require('../../utils/utils.js');
-const { isNotEmpty, isChinese } = utils;
+import {
+  LETTERS,
+  CITY_ARR_TWO as cityList
+} from '../../locale/citydata'
+import utils from '../../utils/utils'
+import config from '../../utils/config'
+
+const { isNotEmpty, isChinese, safeGet, getCityListSortedByInitialLetter, getLocationUrl, getCountyListUrl, getIndexUrl } = utils;
 const appInstance = getApp();
 
 Page({
   data: {
-    searchLetter: [],
-    showLetter: "",
+    sideBarLetterList: [],
     winHeight: 0,
     cityList: [],
-    isShowLetter: false,
+    showChosenLetterToast: false,
     scrollTop: 0,//置顶高度
     scrollTopId: '',//置顶id
     city: "定位中",
     currentCityCode: '',
-    hotcityList: [{ cityCode: 110000, city: '北京市' }, { cityCode: 310000, city: '上海市' }, { cityCode: 440100, city: '广州市' }, { cityCode: 440300, city: '深圳市' }, { cityCode: 330100, city: '杭州市' }, { cityCode: 320100, city: '南京市' }, { cityCode: 420100, city: '武汉市' }, { cityCode: 120000, city: '天津市' }, { cityCode: 610100, city: '西安市' },],
-    commonCityList: [{ cityCode: 110000, city: '北京市' }, { cityCode: 310000, city: '上海市' }],
-    countyList: [{ cityCode: 110000, county: 'A区' }, { cityCode: 310000, county: 'B区' }, { cityCode: 440100, county: 'C区' }, { cityCode: 440300, county: 'D区' }, { cityCode: 330100, county: 'E县' }, { cityCode: 320100, county: 'F县' }, { cityCode: 420100, county: 'G县' }],
     inputName: '',
     completeList: [],
     county: '',
-    condition: false,
+    showCountyPicker: true,
   },
   onLoad: function () {
     // 生命周期函数--监听页面加载
-    const searchLetter = city.searchLetter;
-    const cityList = city.cityList();
+    const cityListSortedByInitialLetter = getCityListSortedByInitialLetter();
     const sysInfo = wx.getSystemInfoSync();
-    console.log(sysInfo);
     const winHeight = sysInfo.windowHeight;
-    const itemH = winHeight / searchLetter.length;
-    let tempArr = [];
-
-    searchLetter.map(
-      (item, index) => {
-        // console.log(item);
-        // console.log(index);
-        let temp = {};
-        temp.name = item;
-        temp.tHeight = index * itemH;
-        temp.bHeight = (index + 1) * itemH;
-        tempArr.push(temp)
-      }
-    );
-    // console.log(tempArr);
+    // const itemH = winHeight / LETTERS.length;
+    // let tempArr = LETTERS.map(
+    //   (item, index) => ({
+    //     name: item,
+    //     tHeight: index * itemH,
+    //     bHeight: (index + 1) * itemH,
+    //   })
+    // );
+    const sideBarLetterList = LETTERS.map(letter => ({ name: letter }));
     this.setData({
-      winHeight: winHeight,
-      itemH: itemH,
-      searchLetter: tempArr,
-      cityList: cityList
+      winHeight,
+      // itemH: itemH,
+      sideBarLetterList,
+      cityList: cityListSortedByInitialLetter
     });
-
+    // 定位
     this.getLocation();
-
   },
   onReady: function () {
     // 生命周期函数--监听页面初次渲染完成
@@ -81,133 +71,106 @@ Page({
 
   },
 
-  clickLetter: function (e) {
-    // console.log(e);
-    console.log(e.currentTarget.dataset.letter)
-    const showLetter = e.currentTarget.dataset.letter;
+  touchSideBarLetter: function (e) {
+    const chosenLetter = safeGet(['currentTarget', 'dataset', 'letter'], e)
+    // const chosenLetter = e.currentTarget.dataset.letter;
     this.setData({
-      toastShowLetter: showLetter,
-      isShowLetter: true,
-      scrollTopId: showLetter,
+      toastShowLetter: chosenLetter,
+      showChosenLetterToast: true,
+      scrollTopId: chosenLetter,
     })
-    // const that = this;
-    // wx.showToast({
-    //   title: showLetter,
-    //   disabled: true,
-    //   duration: 500,
-    //   complete: function() {
-    //     that.setData({
-    //       scrollTopId: showLetter,
-    //     })
-    //   }
-    // })
-    const that = this;
-    setTimeout(function () {
-      that.setData({
-        isShowLetter: false
-      })
-    }, 500)
-  },
-  reGetLocation: function () {
-    appInstance.globalData.defaultCity = this.data.city
-    appInstance.globalData.defaultCounty = this.data.county
-    console.log(appInstance.globalData.defaultCity);
-    //返回首页
-    wx.switchTab({
-      url: '../index/index'
-    })
+    // close toast of chosenLetter
+    setTimeout(() => { this.setData({ showChosenLetterToast: false }) }, 500)
   },
   //选择城市
-  bindCity: function (e) {
-    // console.log("bindCity");
-    // console.log(e);
+  chooseCity: function (e) {
+    const { city, code } = safeGet(['currentTarget', 'dataset'], e)
+    // const { city, code } = e.currentTarget.dataset
     this.setData({
-      condition: true,
-      city: e.currentTarget.dataset.city,
-      currentCityCode: e.currentTarget.dataset.code,
+      showCountyPicker: true,
+      city,
+      currentCityCode: code,
       scrollTop: 0,
       completeList: [],
     })
-    this.selectCounty()
+    this.getCountyList()
 
     appInstance.globalData.defaultCity = this.data.city
     appInstance.globalData.defaultCounty = ''
     console.log(appInstance.globalData.defaultCity)
   },
 
-  bindCounty: function (e) {
-    console.log(e);
-    this.setData({ county: e.currentTarget.dataset.city })
-    appInstance.globalData.defaultCounty = this.data.county
-    console.log(appInstance.globalData.defaultCounty);
-
-    wx.switchTab({
-      url: '../index/index'
-    })
+  chooseCounty: function (e) {
+    const county = safeGet(['currentTarget', 'dataset', 'city'], e)
+    console.log(county)
+    this.setData({ county })
+    appInstance.globalData.defaultCounty = county
+    // 返回首页
+    wx.switchTab({ url: getIndexUrl() })
   },
 
   //点击热门城市回到顶部
   hotCity: function () {
-    console.log("hotCity");
-    this.setData({
-      scrollTop: 0,
-    })
+    this.setData({ scrollTop: 0 })
   },
   bindScroll: function (e) {
-    //  console.log(e.detail)
+    // console.log(e.detail)
   },
-  selectCounty: function () {
-    console.log("正在定位区县");
-    let code = this.data.currentCityCode
+  getCountyList: function () {
+    console.log("正在获取区县");
+    const code = this.data.currentCityCode
 
     wx.request({
-      url: `https://apis.map.qq.com/ws/district/v1/getchildren?&id=${code}&key=${config.key}`,
+      url: getCountyListUrl(code),
       success: res => {
-        this.setData({
-          countyList: res.data.result[0],
-        })
-        console.log("请求区县成功" + `https://apis.map.qq.com/ws/district/v1/getchildren?&id=${code}&key=${config.key}`);
+        const resultArray = safeGet(['data', 'result'], res)
+        const countyList = isNotEmpty(resultArray) ? resultArray[0] : []
+        console.log(countyList)
+        this.setData({ countyList })
       },
       fail: () => { console.log("请求区县失败，请重试") }
     })
   },
   getLocation: function () {
     console.log("正在定位城市");
-    this.setData({
-      county: ''
-    })
-    // const that = this;
+    this.setData({ county: '' })
+
     wx.getLocation({
       type: 'wgs84',
       success: res => {
-        let latitude = res.latitude
-        let longitude = res.longitude
+        const { latitude, longitude } = res
         wx.request({
-          url: `https://apis.map.qq.com/ws/geocoder/v1/?location=${latitude},${longitude}&key=${config.key}`,
+          url: getLocationUrl(latitude, longitude),
           success: res => {
+            const { city, adcode, district } = safeGet(['data', 'result', 'ad_info'], res)
             this.setData({
-              city: res.data.result.ad_info.city,
-              currentCityCode: res.data.result.ad_info.adcode,
-              county: res.data.result.ad_info.district
+              city,
+              currentCityCode: adcode,
+              county: district
             })
-            this.selectCounty();
+            console.log(city)
+            appInstance.globalData.defaultCity = city
+            this.getCountyList();
           }
         })
       }
     })
   },
+  reGetLocation: function () {
+    const { city, county } = this.data
+    appInstance.globalData.defaultCity = city
+    appInstance.globalData.defaultCounty = county
+    console.log(appInstance.globalData.defaultCity);
+    //返回首页
+    wx.switchTab({ url: getIndexUrl() })
+  },
   // 失焦时清空输入框
   bindBlur: function (e) {
-    this.setData({
-      inputName: ''
-    })
+    this.setData({ inputName: '' })
   },
   // 输入框输入时
   bindKeyInput: function (e) {
-    // console.log("input: " + e.detail.value);
-    this.setData({
-      inputName: e.detail.value
-    })
+    this.setData({ inputName: e.detail.value })
     this.associativeSearch()
   },
   // 输入框自动联想搜索
@@ -224,7 +187,6 @@ Page({
     this.showList(resultList)
   },
   searchList: function (str) {
-    const cityList = cityObjs.cityObjs //todo: 导入导出优化
     let targetCity
     return cityList.filter(
       city => {
@@ -235,10 +197,10 @@ Page({
   },
   getTargetCity: function (str, cityObj) {
     if (isChinese(str)) {
-      const slicedChineseName = cityObj.city.slice(0, str.length)
+      const slicedChineseName = cityObj.city && cityObj.city.slice(0, str.length)
       return slicedChineseName
     } else {
-      const slicedPinyinName = cityObj.short.slice(0, str.length).toLowerCase()
+      const slicedPinyinName = cityObj.short && cityObj.short.slice(0, str.length).toLowerCase()
       return slicedPinyinName
     }
     // 在城市数据中，添加简拼到“shorter”属性，就可以实现简拼搜索
@@ -247,13 +209,9 @@ Page({
   showList: function (array) {
     if (isNotEmpty(array)) {
       let finalCityList = array.map(item => ({ city: item.city, code: item.code }))
-      this.setData({
-        completeList: finalCityList,
-      })
+      this.setData({ completeList: finalCityList })
     } else {
-      this.setData({
-        completeList: [{ city: '无匹配城市', code: "000" }]
-      })
+      this.setData({ completeList: [{ city: '无匹配城市', code: "000" }] })
     }
   },
 })
--- utils/utils.js
+++ utils/utils.js
@@ -1,8 +1,38 @@
+import {
+  LETTERS,
+  CITY_ARR_ONE as cityObj,
+} from '../locale/citydata'
+import config from 'config'
+
+// 城市名按首字母分组
+const getCityListSortedByInitialLetter = () => (
+  LETTERS.map(
+    letter => ({
+      initial: letter,
+      cityInfo: cityObj.filter(city => city.initial == letter)
+    })
+  )
+)
+
 const isNotEmpty = array => (Array.isArray(array) && array.length > 0)
 
 const isChinese = str => (/^[\u4e00-\u9fa5]+$/.test(str))
 
-module.exports = {
+// 安全地在深层嵌套对象中取值
+const safeGet = (keyList, obj) => keyList.reduce((preValue, curKey) => ((preValue && preValue[curKey]) ? preValue[curKey] : null), obj)
+
+// API
+const getLocationUrl = (latitude, longitude) => (`https://apis.map.qq.com/ws/geocoder/v1/?location=${latitude},${longitude}&key=${config.key}`)
+const getCountyListUrl = code => (`https://apis.map.qq.com/ws/district/v1/getchildren?&id=${code}&key=${config.key}`)
+const getIndexUrl = () => ('../index/index')
+
+// module.exports = {
+export default {
   isNotEmpty,
-  isChinese
+  isChinese,
+  safeGet,
+  getCityListSortedByInitialLetter,
+  getLocationUrl,
+  getCountyListUrl,
+  getIndexUrl
 }
